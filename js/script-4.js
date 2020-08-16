d98b5c26f3ccdc79348887878f164122f8bfc166
Alexander Khmyzenko <alexander193800@gmail.com>
2020-08-16 23:00:59 +0200

    Суммирование значений свойств объекта

--- js/script-4.js
+++ js/script-4.js
@@ -1,38 +1,31 @@
-// Форматирование строки в зависимости от длинны строки
+// Суммирование значений свойств объекта
+// Напиши функцию countTotalSalary(employees) принимающую объект зарплат.
+// Функция считает общую сумму зарплаты работников и возвращает ее.
+// Каждое поле объекта, передаваемого в функцию, имеет вид "имя": "зарплата".
 
-// Напиши функцию formatString(string, maxLength = 40)
-// которая принимает строку и форматирует ее если необходимо.
-
-// Если длина строки не превышает maxLength, функция возвращает ее в исходном виде.
-// Если длина больше maxLength, то функция обрезает строку до размера maxLength символов
-// и добавляет в конец строки троеточие ...,
-// после чего возвращает укороченную версию.
-
-const formatString = function (string = "", maxLength = 40) {
-  let message = string.split("");
-  let message1;
-  let result;
-
-  if (string.length < maxLength) {
-    message = string;
-    result = message;
-    // console.log(message);
-  } else if (maxLength <= 30) {
-    message = message.slice(0, 30);
-    message1 = message.splice(31, 0, ".", ".", ".");
-    result = message.join("");
-  } else {
-    message = message.slice(0, 40);
-    message1 = message.splice(41, 0, ".", ".", ".");
-    result = message.join("");
+const countTotalSalary = function (employees) {
+  "use strict";
+  let total = 0;
+  const values = Object.values(employees);
+  for (const value of values) {
+    total += value;
   }
-  return result;
+  return total;
 };
 
-console.log(formatString("Curabitur ligula sapien, tincidunt non."));
+// Объекты и ожидаемый результат
+const developers = {
+  mango: 300,
+  poly: 250,
+  alfred: 450,
+};
+console.log(countTotalSalary(developers));
+// 1000
 
-console.log(formatString("Vestibulum facilisis, purus nec pulvinar iaculis."));
-
-console.log(
-  formatString("Vestibulum facilisis, purus nec pulvinar iaculis.", 30)
-);
+const supports = {
+  kiwi: 200,
+  lux: 150,
+  chelsy: 150,
+};
+console.log(countTotalSalary(supports));
+// 500
